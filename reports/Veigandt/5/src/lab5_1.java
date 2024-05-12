3e0be7ffbcdecabcafb85dc84adbdbaee414dee3
Ilya Veigandt <30446448+ilya4664@users.noreply.github.com>
2024-05-12 21:47:12 +0300

    (Veigandt) lab5

---
+++ reports/Veigandt/5/src/lab5_1.java
@@ -0,0 +1,27 @@
+interface Building{
+    void Open();
+    void Close();
+}
+abstract class PublicBuilding implements Building{
+    @Override
+    public void Open() {
+        System.out.println("Здание открыто");
+    }
+    @Override
+    public void Close() {
+        System.out.println("Здание закрыто");
+    }
+}
+class Theater extends PublicBuilding{
+    public void doEvent(){
+        System.out.println("Проходит репетиция");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Theater theater = new Theater();
+        theater.Open();
+        theater.doEvent();
+        theater.Close();
+    }
+}
